dea49ac2bc95301948e468bc330c4df609a487c7
Kevin Alves <kevin.alves.dev@gmail.com>
2023-03-06 20:24:46 -0300

    initial backbone

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const { PORT } = process.env;
+
+app.use(cors());
+app.use(json());
+app.use(helmet());
+
+app.get('/health-check', (req, res) => res.send());
+
+app.use();
+
+if (process.env.NODE_ENV === 'production') {
+  await import('./database/migrate.js');
+}
+
+app.listen(PORT, () => {
+  console.log(chalk.green(`Server listening on port ${PORT}`));
+});
