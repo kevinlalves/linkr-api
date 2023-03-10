610e1c5d423310b44a5260069ef25270d2529d17
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-10 12:25:53 -0300

    :recycle: post edited filter by hashtag

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -26,14 +26,23 @@ export const getPosts = () =>
 export const getPostsByHashtag = (hashtag) => {
   return db.query(
     `
-    SELECT posts.*, COALESCE(COUNT(likes.id), 0) as likes, users.username, users.picture_url from posts
-    LEFT JOIN likes ON likes.post_id = posts.id
-    JOIN users ON posts.user_id = users.id
+    SELECT
+        p.id AS "postId",
+        p.content AS content,
+        p.shared_url AS "sharedUrl",
+        p.created_at AS "createdAt",
+        u.username AS "username",
+        u.picture_url AS "pictureUrl",
+        coalesce(count(l.id), 0) AS "likesCount"
+    FROM posts p
+    JOIN users u ON p.user_id = u.id
+    LEFT JOIN likes l on p.id = l.post_id
     JOIN hashtag_posts ON hashtag_posts.post_id = posts.id
     JOIN hashtags ON hashtags.id = hashtag_posts.hashtag_id
     WHERE hashtags.name = $1
-    GROUP BY posts.id, users.username, users.picture_url
-    ORDER BY posts.created_at desc;
+    GROUP BY "postId", content, "sharedUrl", "createdAt", username, "pictureUrl"
+    ORDER BY "createdAt" DESC
+    LIMIT 20;
     `,
     [hashtag]
   );
