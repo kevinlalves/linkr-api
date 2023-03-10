988ffd33acf16c918b16ba50eded34bb64cc2624
Paula Rodrigues <pullynnhah@icloud.com>
2023-03-10 12:29:55 -0300

    :recycle: hashtags most mentioned

--- src/repositories/hashtags.repository.js
+++ src/repositories/hashtags.repository.js
@@ -1,5 +1,14 @@
-import db from "../database/database.connection.js";
+import db from '../database/database.connection.js';
 
 export const getFirstTenMostMentionedHashtags = () => {
-  return db.query('SELECT name FROM hashtags ORDER BY mentions LIMIT 10');
+  return db.query(
+    `
+    SELECT h.name, COUNT(hp.post_id) count
+    FROM hashtags h
+    JOIN hashtag_posts hp on h.id = hp.hashtag_id
+    GROUP BY h.name
+    ORDER BY count
+    LIMIT 10;
+    `
+  );
 };
